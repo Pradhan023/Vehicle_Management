e0f29e28bacb0f0ba2f15f0d15b4f434cb12b38d
Anish Pradhan <anishpradha5523@gmail.com>
2025-05-09 10:10:54 +0530

    final commit

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -7,8 +7,6 @@ export const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
 });
 
 // Test connection
